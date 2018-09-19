b87526858c32ca8835bb1db718621dc4ad75dcb8
Kai Joseph <kjoseph21@choate.edu>
2018-09-18 20:45:30 -0400

    Text Adventure Beginning Plot
    I have written the beginning plot of the text adventure.

---
+++ Story.py
@@ -0,0 +1,52 @@
+
+def intro:
+
+	name = input("\nGreetings, traveller. What is your name?\n")
+
+	print('''
+
+	You didn't think that it would ever be possible.
+
+	Up until this point, you were in heavy denial that you would ever reach such a destination.
+
+	Yet, now that you have witnessed for yourself the grand domes and buildings hanging over the city walls,
+
+	the gates of reality have suddenly opened.
+
+	Welcome,''',name+''', to the glorious ancient city of Xenloch.
+
+	''')
+
+def beginning:
+
+	print('''
+
+	<BEGINNING>
+
+	As you enter the city walls, you cannot help but admire the glorious construction of the city facilities.
+
+	Xenloch is a city from a time when society was civilised, artistic, intelligent.
+
+	Now humanity has been reduced to a barbaric collection of endless wars, violence, and destruction.
+
+	Society has been destroyed almost to the extent of the ruins of Xenloch.
+
+	Xenloch, once a glorious and bustling city, now merely a collection of destroyed bricks and ancient buildings, barely standing.
+
+	As you walk into the abandoned city square, you suddenly hear a distant voice.
+
+	"Hello? Is anyone there?"
+
+	As you begin to walk away, you hear the voice again, louder this time.
+
+	"I have been waiting sixty years for you!"
+
+	You start to hear footsteps, but cannot deduct the source of them. Suddenly, an old man in white robes appears in front of you,
+
+	staring into your eyes.
+
+	"You have finally arrived!" he says.
+
+	''')
+
+	
