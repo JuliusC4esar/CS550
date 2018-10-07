8aee5a33d15b2076a9daa9715cf0a2db38a4d226
Kai Joseph <kjoseph21@choate.edu>
2018-10-07 17:00:10 -0400

    Minesweeper almost complete
    All minesweeper functions except for zero recursion and the flag mechanic have been completed.

--- minesweeper.py
+++ minesweeper.py
@@ -4,6 +4,88 @@
 # Enter 3 integers: a width, a height, and a number of bombs, and a minesweeper board will be generated.
 
 
+def zero():
+
+	for x in range(height):
+
+		for y in range(width-1):
+
+			if hidden[x][y] == 0:
+
+				if x+1 < height:
+
+					hidden[x+1][y] = board[x+1][y]
+
+					if hidden[x+1][y] == 0:
+
+						zero() 	
+
+				if x-1 >= 0:
+
+					hidden[x-1][y] = board[x-1][y]
+
+					if hidden[x-1][y] == 0:
+
+						zero()
+
+				if x+1 < height and y+1 < width:
+
+					hidden[x+1][y+1] = board[x+1][y+1]
+
+					if hidden[x+1][y+1] == 0:
+
+						zero()
+
+				if x-1 >= 0 and y+1 < width:
+
+					hidden[x-1][y+1] = board[x-1][y+1]
+
+					if hidden[x-1][y+1] == 0:
+
+						zero()
+
+				if x-1 >= 0 and y-1 >= 0:
+
+					hidden[x-1][y-1] = board[x-1][y-1]
+
+					if hidden[x-1][y-1] == 0:
+
+						zero()
+
+				if y+1 < width:
+
+					hidden[x][y+1] = board[x][y+1]
+
+					if hidden[x][y+1] == 0:
+
+						zero()
+
+				if y-1 >= 0:
+
+					hidden[x][y-1] = board[x][y-1]
+
+					if hidden[x][y-1] == 0:
+
+						zero()
+
+				if x+1 < height and y-1 >= 0:
+
+					hidden[x+1][y-1] = board[x+1][y-1]
+
+					if hidden[x+1][y-1] == 0:
+
+						zero()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -21,93 +103,126 @@ try:
 
 	bombs = int(sys.argv[3])
 
-	board = [[0] for x in range(height)]
 
-	
-	for x in range(height):
+	if width*height >= 25 and width*height >= bombs:
 
-		for y in range(width-1):
+		board = [[0] for x in range(height)]
 
-			board[x].append(0)
+		
+		for x in range(height):
 
+			for y in range(width-1):
 
-
-	for x in range(height):
-
-		print(*board[x])
+				board[x].append(0)
 
 
-	for b in range(bombs):
+		hidden = [['?'] for x in range(height)]
 
-		x = random.randint(0,height-1)
+		for x in range(height):
 
-		y = random.randint(0,width-1)
+			for y in range(width-1):
 
-		if board[x][y] != '*':
-
-			board[x][y] = '*'
-
-		else:
-
-			b = b-1
+				hidden[x].append('?')
 
 
-		if board[x][y] == '*':
 
-			if x+1 < height:
 
-				if board[x+1][y] != '*':
+
+
+		b = 0         # Number generation and bomb generation
+
+		while b < bombs:    
+
+			x = random.randint(0,height-1)
+
+			y = random.randint(0,width-1)
+
+			if board[x][y] != '*':
+
+				board[x][y] = '*'
+
+				if x+1 < height:
+
+					if board[x+1][y] != '*':
+				
+						board[x+1][y] += 1
+
+				if x-1 >= 0:
+
+					if board[x-1][y] != '*':
+
+						board[x-1][y] += 1
+
+				if x+1 < height and y+1 < width:
+
+					if board[x+1][y+1] != '*':
+
+						board[x+1][y+1] += 1
+
+				if x-1 >= 0 and y+1 < width:
+
+					if board[x-1][y+1] != '*':
+
+						board[x-1][y+1] += 1
+
+				if x-1 >= 0 and y-1 >= 0:
+
+					if board[x-1][y-1] != '*':
+
+						board[x-1][y-1] += 1
+
+				if y+1 < width:
+
+					if board[x][y+1] != '*':
+
+						board[x][y+1] += 1
+
+				if y-1 >= 0:
+
+					if board[x][y-1] != '*':
+
+						board[x][y-1] += 1
+
+				if x+1 < height and y-1 >= 0:
+
+					if board[x+1][y-1] != '*':
+
+						board[x+1][y-1] += 1
+
+			elif board[x][y] == '*':
+
+				b = b-1
+
+			b = b + 1
+
+
+
 			
-					board[x+1][y] += 1
 
-			if x-1 >= 0:
+			# Initial Reveal
 
-				if board[x-1][y] != '*':
+			initialx = int(width/2)
 
-					board[x-1][y] += 1
+			initialy = int(height/2)
 
-			if x+1 < height and y+1 < width:
+			hidden[initialy][initialx] = board[initialy][initialx]
 
-				if board[x+1][y+1] != '*':
+			hidden[initialy+1][initialx] = board[initialy+1][initialx]
 
-					board[x+1][y+1] += 1
+			hidden[initialy-1][initialx] = board[initialy-1][initialx]
 
-			if x-1 >= 0 and y+1 < width:
+			hidden[initialy][initialx+1] = board[initialy][initialx+1]
 
-				if board[x-1][y+1] != '*':
-
-					board[x-1][y+1] += 1
-
-			if x-1 >= 0 and y-1 >= 0:
-
-				if board[x-1][y-1] != '*':
-
-					board[x-1][y-1] += 1
-
-			if y+1 < width:
-
-				if board[x][y+1] != '*':
-
-					board[x][y+1] += 1
-
-			if y-1 >= 0:
-
-				if board[x][y-1] != '*':
-
-					board[x][y-1] += 1
-
-			if x+1 < height and y-1 >= 0:
-
-				if board[x+1][y-1] != '*':
-
-					board[x+1][y-1] += 1
+			hidden[initialy][initialx-1] = board[initialy][initialx-1]
 
 
-	print()
 
-	for x in range(height):
 
-		print(*board[x])
+		print()  # Print final board
+
+		for x in range(height):
+
+			print(*hidden[x])
 
 
 
@@ -115,59 +230,102 @@ try:
 
 
 
-	# Input starts here:
 
 
-	while True:
-
-			while True:
-
-				try:
-
-					xpos = int(input("\nEnter the x-coordinate of the item you wish to select. (The bottom left corner has coordinates (1,1), so coordinates start at 1 and go up with the array.)\n"))
-
-					ypos = int(input("\nEnter the y-coordinate of the item you wish to select. (The bottom left corner has coordinates (1,1), so coordinates start at 1 and go up with the array.)\n"))
-
-					if xpos >= 1 and xpos <= width and ypos >= 1 and ypos <= height:
-
-						break
-
-					else:
-
-						print("\nPlease enter an integer that is greater than or equal to one and is within the parameters of the array.\n")
-
-				except ValueError:
-
-					print("\nPlease enter an integer that is greater than or equal to one and is within the parameters of the array.\n")
+		# Input starts here:
 
 
+		while True:
 
+				while True:
 
-			if board[(height-(ypos-1))-1][xpos-1] == '*':
+					try:
 
-				print("\nYou selected a mine! You lose!")
+						xpos = int(input("\nEnter the x-coordinate of the region that you wish to reveal.\n"))
 
-				quit()
+						ypos = int(input("\nEnter the y-coordinate of the region that you wish to reveal.\n"))
 
-			else:
+						if xpos >= 0 and xpos <= width and ypos >= 0 and ypos <= height:
 
-				print("\nYou selected the number:",board[(height-(ypos-1))-1][xpos-1])
+							break
 
+						else:
 
-			for x in range(height):
+							print("That number doesn't work! Select a proper one, yeah?")
 
-				print(*board[x])
+					except ValueError:
+
+						print("\nThat number doesn't work! Select a proper one, yeah?\n")
 
 
 
 
+				if board[ypos][xpos] == '*': # Lose
+
+					print("\nYou selected a mine! You lose! Here are all the positions of the mines!")
+
+					for x in range(height):
+
+						for y in range(width):
+
+							if board[x][y] == '*':
+
+								hidden[x][y] = board[x][y]
 
 
-except ValueError:
+					for x in range(height): # Print board
 
-	print("Please enter 3 integers: A width, a height, and a number of bombs.")
+						print(*board[x])
+
+
+
+					quit()
+
+
+
+				else:
+
+					print("\nYou landed on the number:",board[ypos][xpos])
+
+					hidden[ypos][xpos] = board[ypos][xpos]
+
+					if hidden[ypos][xpos] == 0:
+
+						zero()
+
+				for x in range(height): # Print board
+
+					print(*hidden[x])
+
+
+				if board.count('*') == hidden.count('?'): # Win condition
+
+					print("\nYou have cleared the board! You win!")
+
+					quit()
+
+
+
+	else:  # Max bomb limit
+
+		print("\nThis number of bombs does not work with the board size. You cannot have",bombs,"bombs with",str(width*height),"square units board size!")
+
+		quit()
+
+except ValueError: # Enter three integers into the console
+
+	print("\nPlease enter 3 integers: A width, a height, and a number of bombs.")
 
 	quit()
+
+
+
+
+
+
+
+
+
 
 
 
