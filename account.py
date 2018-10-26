96d0d76cf0d44437d1606518c22547cde407f0b6
Kai Joseph <kjoseph21@choate.edu>
2018-10-26 14:00:31 -0400

    some bugfixes for the account

--- account.py
+++ account.py
@@ -28,13 +28,40 @@ class Account:
 
 		if intake > int(self.balance):
 
-			print("You can't withdraw that much money! Your balance:",balance)
+			print("You can't withdraw that much money! Your balance:",self.balance)
 
 		else:
 
 			self.balance = int(self.balance) - intake
 
 			print(intake,"dollars withdrawn. Your balance is now",self.balance)
+
+
+	def deposit(self):
+
+		while True:
+
+			intake = input("\nHow much money would you like to deposit?\n")
+
+			try:
+
+				intake = int(intake)
+
+				break
+
+			except ValueError:
+
+				print("That's not a number. (do not include spaces or letters or special characters. only include integers.)")
+
+
+		self.balance = int(self.balance) + intake
+
+		print(intake,"dollars deposited. Your balance is now",str(self.balance))
+
+
+	def num(self):
+
+		print("Your account number is:",self.number)
 
 
 
@@ -49,11 +76,25 @@ if password == str(account.pin):
 
 	while True:
 
-		action = input("Action: ")
+		action = input("Action (withdraw/deposit/number/exit): ")
 
 		if action.lower() == "withdraw":
 
 			account.withdraw()
+
+		elif action.lower() == "deposit":
+
+			account.deposit()
+
+		elif action.lower() == "number":
+
+			account.num()
+
+		elif action.lower() == "exit":
+
+			print("Thank you for using this ATM! Goodbye.")
+
+			quit()
 
 
 else:
